30607ed91c57f4fe9c7f52d41ea61ab1b0615aeb
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-31 12:28:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def help_
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -9,5 +21,9 @@ def help_
 end
 
 def list
-
+  count = 1
+  songs.map do |song|
+    puts "#{count}. #{song}"
+    count += 1
+  end
 end
