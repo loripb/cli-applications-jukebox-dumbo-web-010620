75052af0c8bfcfe69ef0c63bdbbdb3e9daead309
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-31 12:24:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def _help
+def help_
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
