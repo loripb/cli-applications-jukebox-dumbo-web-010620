18d68ee4609f5cd94bbc0df3dd05dbb49eca36c4
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-31 12:22:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,8 +1,8 @@
-def say_hello(name)
-  "Hi #{name}!"
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : eits thid program"
 end
-
-puts "Enter your name:"
-users_name = gets.strip
-
-puts say_hello(users_name)
