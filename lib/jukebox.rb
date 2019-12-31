9f4945f3378830b9d4eae139c73833323138be2a
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-31 12:55:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,7 +3,7 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit : eits thid program"
+  puts "- exit : exits thid program"
 end
 
 def list(songs)
